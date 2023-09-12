505103a966c319ebc58cbe04c584df80d78222b8
Chris Deck <deckchris95@gmail.com>
2023-09-12 15:56:48 -0400

    fixes

--- main.py
+++ main.py
@@ -1,11 +1,126 @@
+# import os
+# import discord
+# from dotenv import load_dotenv
+# from discord.ext import commands
+# import requests
+# import mysql.connector
+# import mysql.connector.pooling  # Import the pooling module
+# import asyncio
+
+# # Credentials
+# load_dotenv()
+# TOKEN = os.getenv('DISCORD_TOKEN')
+# USER = os.getenv('RDS_USER')
+# PASS = os.getenv('RDS_PASSWORD')
+# HOST = os.getenv('RDS_HOST')
+
+# # print(HOST)
+# # print(USER)
+
+
+# # Define a function to create a MySQL connection pool
+# def create_connection_pool():
+#     return mysql.connector.pooling.MySQLConnectionPool(
+#         pool_name="my_pool",
+#         pool_size=5,  # You can adjust this value as needed
+#         user=USER,
+#         password=PASS,
+#         host=HOST,
+#         database='discgolfdb'
+#     )
+
+# # Initialize the connection pool
+# connection_pool = create_connection_pool()
+
+# # Function to get a MySQL connection from the pool
+# def get_mysql_connection():
+#     return connection_pool.get_connection()
+
+# # Function to ping the MySQL server periodically to keep the connection alive
+# def ping_mysql_server():
+#     while True:
+#         try:
+#             connection = get_mysql_connection()
+#             connection.ping(reconnect=True)
+#             connection.close()
+#         except mysql.connector.Error as err:
+#             print(f"Error pinging MySQL server: {err}")
+#         time.sleep(3600)  # Ping every hour
+
+# # Start a separate thread to ping the MySQL server
+# import threading
+# import time
+
+# ping_thread = threading.Thread(target=ping_mysql_server)
+# ping_thread.daemon = True
+# ping_thread.start()
+
+# #conn = mysql.connector.connect(db_config)
+# conn = mysql.connector.connect(
+#     host=HOST,
+#     user=USER,
+#     password=PASS,
+#     database='discgolfdb'
+# )
+# cursor = conn.cursor()
+# # # Create a table to store user bags
+# cursor.execute('''CREATE TABLE IF NOT EXISTS bags (
+#                 user_id TEXT,
+#                 disc_name TEXT,
+#                 brand TEXT,
+#                 plastic TEXT,
+#                 speed REAL,
+#                 glide REAL,
+#                 turn REAL,
+#                 fade REAL
+#                 )''')
+# conn.commit()
+
+# # Function to get a MySQL connection from the pool with reconnection logic
+# def get_mysql_connection():
+#     try:
+#         connection = connection_pool.get_connection()
+#         connection.ping(reconnect=True)
+#         return connection
+#     except mysql.connector.Error as err:
+#         print(f"Error getting MySQL connection: {err}")
+#         return None
+
+# # Function to execute MySQL queries with retries
+# def execute_mysql_query(query, *args):
+#     max_retries = 3
+#     retries = 0
+#     while retries < max_retries:
+#         try:
+#             connection = get_mysql_connection()
+#             if connection is not None:
+#                 cursor = connection.cursor()
+#                 cursor.execute(query, args)
+#                 connection.commit()
+#                 cursor.close()
+#                 connection.close()
+#                 return
+#         except mysql.connector.Error as err:
+#             print(f"Error executing MySQL query: {err}")
+#             retries += 1
+#     print(f"Max retries reached for query: {query}")
+#     raise Exception("Failed to execute MySQL query after max retries")
+
+
+# # Connect to the SQLite database
+# conn = sqlite3.connect('bag_data.db')
+# cursor = conn.cursor()
+
 import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 import requests
 import mysql.connector
-import mysql.connector.pooling  # Import the pooling module
+import mysql.connector.pooling
 import asyncio
+import threading
+import time
 
 # Credentials
 load_dotenv()
@@ -14,95 +129,45 @@ USER = os.getenv('RDS_USER')
 PASS = os.getenv('RDS_PASSWORD')
 HOST = os.getenv('RDS_HOST')
 
-# print(HOST)
-# print(USER)
-
 # Create bot
 intents = discord.Intents.all();
 bot = commands.Bot(command_prefix='!', intents=intents, case_insensitive=True)
 
-# Define a function to create a MySQL connection pool
-def create_connection_pool():
-    return mysql.connector.pooling.MySQLConnectionPool(
-        pool_name="my_pool",
-        pool_size=5,  # You can adjust this value as needed
-        user=USER,
-        password=PASS,
-        host=HOST,
-        database='discgolfdb'
-    )
-
-# Initialize the connection pool
-connection_pool = create_connection_pool()
-
-# Function to get a MySQL connection from the pool
-def get_mysql_connection():
-    return connection_pool.get_connection()
-
-# Function to ping the MySQL server periodically to keep the connection alive
-def ping_mysql_server():
-    while True:
-        try:
-            connection = get_mysql_connection()
-            connection.ping(reconnect=True)
-            connection.close()
-        except mysql.connector.Error as err:
-            print(f"Error pinging MySQL server: {err}")
-        time.sleep(3600)  # Ping every hour
-
-# Start a separate thread to ping the MySQL server
-import threading
-import time
-
-ping_thread = threading.Thread(target=ping_mysql_server)
-ping_thread.daemon = True
-ping_thread.start()
-
-#conn = mysql.connector.connect(db_config)
-conn = mysql.connector.connect(
-    host=HOST,
-    user=USER,
-    password=PASS,
-    database='discgolfdb'
-)
-cursor = conn.cursor()
-# # Create a table to store user bags
-cursor.execute('''CREATE TABLE IF NOT EXISTS bags (
-                user_id TEXT,
-                disc_name TEXT,
-                brand TEXT,
-                plastic TEXT,
-                speed REAL,
-                glide REAL,
-                turn REAL,
-                fade REAL
-                )''')
-conn.commit()
-
-# Function to get a MySQL connection from the pool with reconnection logic
-def get_mysql_connection():
+# Create a function to create a MySQL connection
+def create_mysql_connection():
     try:
-        connection = connection_pool.get_connection()
-        connection.ping(reconnect=True)
+        connection = mysql.connector.connect(
+            host=HOST,
+            user=USER,
+            password=PASS,
+            database='discgolfdb'
+        )
         return connection
     except mysql.connector.Error as err:
-        print(f"Error getting MySQL connection: {err}")
+        print(f"Error creating MySQL connection: {err}")
         return None
+
+# Initialize the MySQL connection
+conn = create_mysql_connection()
+cursor = conn.cursor()
 
 # Function to execute MySQL queries with retries
 def execute_mysql_query(query, *args):
+    global conn  # Declare conn as a global variable
+    global cursor  # Declare cursor as a global variable
     max_retries = 3
     retries = 0
     while retries < max_retries:
         try:
-            connection = get_mysql_connection()
-            if connection is not None:
-                cursor = connection.cursor()
+            if conn.is_connected():
                 cursor.execute(query, args)
-                connection.commit()
-                cursor.close()
-                connection.close()
-                return
+                conn.commit()
+                return  # Remove cursor.close() from here
+            else:
+                # Reconnect if the connection is lost
+                print("Reconnecting to MySQL...")
+                conn = create_mysql_connection()
+                cursor = conn.cursor()
         except mysql.connector.Error as err:
             print(f"Error executing MySQL query: {err}")
             retries += 1
@@ -110,10 +175,30 @@ def execute_mysql_query(query, *args):
     raise Exception("Failed to execute MySQL query after max retries")
 
 
-# # Connect to the SQLite database
-# conn = sqlite3.connect('bag_data.db')
-# cursor = conn.cursor()
-
+# Function to execute MySQL queries with retries for SELECT queries
+def execute_select_query(query, *args):
+    global conn  # Declare conn as a global variable
+    global cursor  # Declare cursor as a global variable
+    max_retries = 3
+    retries = 0
+    while retries < max_retries:
+        try:
+            if conn.is_connected():
+                cursor.execute(query, args)
+                results = cursor.fetchall()  # Fetch the results
+                conn.commit()
+                cursor.close()  # Close the cursor after fetching results
+                return results
+            else:
+                # Reconnect if the connection is lost
+                print("Reconnecting to MySQL...")
+                conn = create_mysql_connection()
+                cursor = conn.cursor()
+        except mysql.connector.Error as err:
+            print(f"Error executing MySQL query: {err}")
+            retries += 1
+    print(f"Max retries reached for query: {query}")
+    raise Exception("Failed to execute MySQL query after max retries")
 
 
 # Startup Information
@@ -123,13 +208,71 @@ async def on_ready():
     print('Bot ID: {}'.format(bot.user.id))
 
 
+# # Function to format and display a user's bag as an embedded message
+# def format_bag(user_id, ctx):
+#     try:
+#         cursor.execute('''SELECT * FROM bags WHERE user_id = %s''', (user_id,))
+#         bag_data = cursor.fetchall()
+
+#         if not bag_data:
+#             return discord.Embed(description="Your bag is empty.")
+
+#         bag = {
+#             'Distance Drivers': [],
+#             'Fairway Drivers': [],
+#             'Mid-Ranges': [],
+#             'Putt/Approach': []
+#         }
+
+#         # Sort the bag_data by speed in descending order (highest speed first)
+#         bag_data.sort(key=lambda x: x[4], reverse=True)
+
+#         for disc in bag_data:
+#             speed = disc[4]
+#             if speed > 8:
+#                 bag['Distance Drivers'].append(disc[1])  # Add disc name to the category
+#             elif speed > 5:
+#                 bag['Fairway Drivers'].append(disc[1])
+#             elif speed > 4:
+#                 bag['Mid-Ranges'].append(disc[1])
+#             else:
+#                 bag['Putt/Approach'].append(disc[1])
+
+#         embed = discord.Embed(title=f"{ctx.author.display_name}'s Bag")
+#         # Check if the user has a profile picture and set the thumbnail accordingly
+#         if ctx.author.avatar:
+#             embed.set_thumbnail(url=ctx.author.avatar.url)
+#         else:
+#             # If the user doesn't have a profile picture, you can set it to a default image or the bot's avatar
+#             embed.set_thumbnail(url=bot.user.avatar.url)
+
+#         for category, discs in bag.items():
+#             if discs:
+#                 embed.add_field(name=category, value=', '.join(discs), inline=False)  # Add a field for each category
+
+#         return embed
+#     except Exception as e:
+#         return discord.Embed(description=f"An error occurred: {str(e)}")
+
+# # Command to display user's bag
+# @bot.command(name='mybag')
+# async def mybag(ctx):
+#     try:
+#         user_id = ctx.author.id
+#         embed = format_bag(user_id, ctx)
+
+#         # Send the embedded message
+#         await ctx.send(embed=embed)
+#     except Exception as e:
+#         await ctx.send(f"An error occurred: {str(e)}")
+
 # Function to format and display a user's bag as an embedded message
 def format_bag(user_id, ctx):
     try:
-        cursor.execute('''SELECT * FROM bags WHERE user_id = %s''', (user_id,))
-        bag_data = cursor.fetchall()
-
-        if not bag_data:
+        query = '''SELECT * FROM bags WHERE user_id = %s'''
+        result = execute_select_query(query, user_id)
+        
+        if not result:
             return discord.Embed(description="Your bag is empty.")
 
         bag = {
@@ -140,9 +283,9 @@ def format_bag(user_id, ctx):
         }
 
         # Sort the bag_data by speed in descending order (highest speed first)
-        bag_data.sort(key=lambda x: x[4], reverse=True)
+        result.sort(key=lambda x: x[4], reverse=True)
 
-        for disc in bag_data:
+        for disc in result:
             speed = disc[4]
             if speed > 8:
                 bag['Distance Drivers'].append(disc[1])  # Add disc name to the category
@@ -180,6 +323,7 @@ async def mybag(ctx):
         await ctx.send(embed=embed)
     except Exception as e:
         await ctx.send(f"An error occurred: {str(e)}")
+
 
 # Function to format and display a user's bag with details in an embedded message
 def format_bag_detailed(user_id, ctx):
